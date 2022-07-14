7715a4699c3798ab7a853222f7bad74de46e0c1e
Christopher Bellanger <christopher.bellanger@teester.com>
2022-07-14 12:31:02 +0200

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import TodoDetail from "./pages/TodoDetail";
+import TodoList from "./pages/TodoList";
 
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<TodoList />} />
+        <Route path="/todo/:id" element={<TodoDetail />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/TodoDetail.jsx
@@ -0,0 +1,3 @@
+const TodoDetail = () => null;
+
+export default TodoDetail;
